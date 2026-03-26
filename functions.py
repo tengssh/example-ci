0c70c01aea7550d89a4272fb49bb80e60bcce656
Shenghan <79361599+tengssh@users.noreply.github.com>
2026-03-26 12:24:26 +0100

    Add temperature check in conversion function

--- functions.py
+++ functions.py
@@ -8,4 +8,6 @@ def multiply(a, b):
     return a * b
 
 def convert_fahrenheit_to_celsius(fahrenheit):
+    if fahrenheit < 459.67:
+        raise AssertionError("This temperature is lower than 0K!")
     return multiply(subtract(fahrenheit, 32), 5 / 9)
